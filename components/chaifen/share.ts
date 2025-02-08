a7f19a81f177122c80cc379e20a42f647a6133db
ZHU Yuhao 朱宇浩 <dr.yuhao.zhu@outlook.com>
2025-02-08 23:45:38 +0100

    Update developing log
    Update developing log
    
    Update 皿
    
    Update stats
    
    Update doc
    
    translate

--- components/chaifen/share.ts
+++ components/chaifen/share.ts
@@ -8,7 +8,7 @@ Author: 朱宇浩 (forFudan) <dr.yuhao.zhu@outlook.com>
 Github: https://github.com/forFudan/
 
 LICENSE 版權聲明：
-專爲宇浩輸入法製作 <https://yuhao.forfudan.com/> <https://yuhao.forfudan.com>
+專爲宇浩輸入法製作 <https://shurufa.com/>
 轉載請保留作者名和出處
 Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International
 ---------------------------------------
